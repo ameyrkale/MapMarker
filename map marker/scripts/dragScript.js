f6cc559012428a9b9984f76bb42f3fa9b457c9d1
ameyrkale <ameyka1@gmail.com>
2017-03-05 19:51:28 -0600

    Add files via upload

---
+++ map marker/scripts/dragScript.js
@@ -0,0 +1,10 @@
+$('#dragThis').draggable(
+    {
+        drag: function(){
+            var offset = $(this).offset();
+            var xPos = offset.left;
+            var yPos = offset.top;
+            $('#posX').text('x: ' + xPos);
+            $('#posY').text('y: ' + yPos);
+        }
+    });
